428f6e12fc99de2cf31299e6ba26059b2496bf03
lucassantos <lukascosta.ls17@gmail.com>
2026-05-07 11:42:50 -0300

    refactor: enhance DidAvailableController and service methods
    - Update filtering logic in DidAvailableController by integrating DidAvailableFilter for improved data handling.
    - Refine API response handling with a new ApiResponse format for consistency.
    - Adjust method signatures in DidAvailableService to accommodate changes in the controller.

--- src/main/java/com/main/numOps/domain/didAvailable/DidAvailableController.java
+++ src/main/java/com/main/numOps/domain/didAvailable/DidAvailableController.java
@@ -1,29 +1,25 @@
 package com.main.numOps.domain.didAvailable;
 
 import com.main.numOps.domain.didAvailable.dtos.DidAvailable;
+import com.main.numOps.domain.didAvailable.dtos.DidAvailableFilter;
 import com.main.numOps.domain.didAvailable.dtos.DidAvailableRangeFilterDTO;
 import com.main.numOps.domain.didAvailable.dtos.DidAvailableRangeUpdateRequest;
-import com.main.numOps.services.FilesUpload.NumberFilesService;
+import com.main.numOps.dtos.ApiResponse;
 import com.main.numOps.utils.responseApi.SucessResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 
 @Tag(name = "DIDs AVAILABLE")
 @RestController
@@ -39,24 +35,36 @@ public class DidAvailableController {
 
     @Operation(summary = "List of available DIDs")
     @GetMapping("/available")
-    public ResponseEntity<Page<DidAvailable>> listByNumbersAvailable(
-            @Parameter(description = "Area", example = "Vila Velha")
-            @RequestParam String area,
-
-            @Parameter(description = "UF", example = "ES")
-            @RequestParam String uf,
-
+    public ResponseEntity<ApiResponse<Page<DidAvailable>>> findAllAvailable(
+            @Valid @ParameterObject DidAvailableFilter filter,
             @PageableDefault(size = 10) Pageable pageable
     ) {
-        return ResponseEntity.ok(
-                didAvailableService.findByNumbersAvailable(area, uf, pageable)
+        return ResponseEntity.ok().body(
+                ApiResponse.success(
+                        didAvailableService.findAllAvailable(filter, pageable),
+                        "Available DIDs found successfully",
+                        HttpStatus.OK.value()
+
+                )
         );
     }
 
     @Operation(summary = "List all DIDs")
     @GetMapping
-    public ResponseEntity<Page<DidAvailableModel>> listAll(Pageable pageable) {
-        return ResponseEntity.ok(didAvailableService.findAll(pageable));
+    public ResponseEntity<ApiResponse<Page<DidAvailableModel>>> listAll(
+            @PageableDefault(
+                    sort = "id",
+                    direction = Sort.Direction.ASC
+            ) Pageable pageable) {
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(
+                        ApiResponse.success(
+                                didAvailableService.findAll(pageable),
+                                "DIDs Found",
+                                HttpStatus.OK.value()
+                        )
+                );
     }
 
     @Operation(summary = "Filter DIDs by range")
@@ -70,42 +78,53 @@ public class DidAvailableController {
 
     @Operation(summary = "Update status in batch")
     @PatchMapping
-    public ResponseEntity<Integer> updateByRange(
+    public ResponseEntity<ApiResponse<Integer>> updateByRange(
             @RequestBody @Valid DidAvailableRangeUpdateRequest dto
     ) {
-        return ResponseEntity.ok(didAvailableService.updateByRange(dto));
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(
+                        didAvailableService.updateByRange(dto),
+                        "Updated DIDs Status",
+                        HttpStatus.OK.value()
+                ));
     }
 
 
     @Operation(
             summary = "Upload DIDs",
             description = """
-        The file must be a .csv or .txt file separated by semicolons.
-
-        ### 📄 Layout do arquivo
-
-        | Campo   | Tipo   | Obrigatório | Descrição              | Exemplo     |
-        |--------|--------|------------|------------------------|------------|
-        | cn     | String | Sim        | Número completo        | 27         |
-        | prefixo| String | Sim        | Prefixo do DID         | 2888       |
-        | mcdu   | String | Sim        | Código MCDU            | 0000       |
-        | area   | String | Sim        | Nome da Cidade         | Serra      |
-        | uf     | String | Sim        | Estado                 | ES         |
-        | status | String | Não        | Status do DID          | AVAILABLE  |
-
-        ### 🧾 Exemplo de arquivo
-
-        ```
-        cn;prefixo;mcdu;area;uf;status
-        27;2888;0000;Serra;ES;AVAILABLE
-        ```
-        """
+                    The file must be a .csv or .txt file separated by semicolons.
+                    
+                    ### 📄 Layout do arquivo
+                    
+                    | Campo   | Tipo   | Obrigatório | Descrição              | Exemplo     |
+                    |--------|--------|------------|------------------------|------------|
+                    | cn     | String | Sim        | Codigo Nacional (DDD)  | 27         |
+                    | prefixo| String | Sim        | Prefixo do DID         | 2888       |
+                    | mcdu   | String | Sim        | Código MCDU            | 0000       |
+                    | area   | String | Sim        | Nome da Cidade         | Serra      |
+                    | uf     | String | Sim        | Estado                 | ES         |
+                    | status | String | Não        | Status do DID          | AVAILABLE  |
+                    
+                    ### 🧾 Exemplo de arquivo
+                    
+                    ```
+                    cn;prefixo;mcdu;area;uf;status
+                    27;2888;0000;Serra;ES;AVAILABLE
+                    ```
+                    """
     )
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<SucessResponse> uploadFile(
+    public ResponseEntity<ApiResponse<Void>> uploadFile(
             @RequestPart("file") MultipartFile file
     ) {
 
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(didAvailableService.uploadFile(file));
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(
+                ApiResponse.success(
+                        didAvailableService.uploadFile(file),
+                        "Upload received. Processing in progress.",
+                        HttpStatus.ACCEPTED.value()
+                        )
+        );
     }
 }
